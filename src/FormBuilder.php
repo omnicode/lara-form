046635d19608127dd84bf762c13db734327e83f6
Arayik Grigoryan <33719273+Aro777@users.noreply.github.com>
2019-02-25 11:58:27 +0400

    Fix small bug in translation system

--- src/FormBuilder.php
+++ src/FormBuilder.php
@@ -502,12 +502,6 @@ class FormBuilder extends BaseFormBuilder
         }
 
         $isTrans = config('lara_form.translator.translate');
-        if (isset($attr['translate'])) {
-            $isTrans = (bool)$attr['translate'];
-        }
-        if (!$isTrans) {
-            return;
-        }
 
         $name = array_shift($arguments);
         $attr = [];
@@ -515,6 +509,15 @@ class FormBuilder extends BaseFormBuilder
         if (!empty($arguments)) {
             $attr = array_shift($arguments);
         }
+        
+        if (isset($attr['translate'])) {
+            $isTrans = (bool)$attr['translate'];
+        }
+        
+        if (!$isTrans) {
+            return;
+        }
+        
         if (empty($this->translator)) {
             $this->translator = app(TranslatorStore::class);
         }
